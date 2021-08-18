f4399a0e22bd30c3de4364786665ebb3cd14a489
erenkaratas77 <erenkaratas@stu.khas.edu.tr>
2021-08-18 16:41:18 +0300

    First commit

---
+++ Oyunla Gelecek/Assets/Scripts/Player.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Player : MonoBehaviour
+{
+    public ParticleSystem teleport;
+    Rigidbody2D myRigidbody;
+    bool isGround = false;
+    bool facingRight = true;
+    [SerializeField] GameObject right;
+    [SerializeField] GameObject left;
+    bool changeGravity = false;
+    [SerializeField] GameObject platformAfterGravityChange;
+
+    [SerializeField]
+    Ease easetipi = Ease.OutBounce;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init();
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!changeGravity)
+        {
+            if (myRigidbody.velocity.x < 0 && facingRight)
+            {
+                facingRight = !facingRight;
+
+                right.gameObject.SetActive(true);
+                left.gameObject.SetActive(false);
+
+
+            }
+            else if (myRigidbody.velocity.x > 0 && !facingRight)
+            {
+                facingRight = !facingRight;
+                right.gameObject.SetActive(false);
+                left.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            if (myRigidbody.velocity.x < 0 && facingRight)
+            {
+                facingRight = !facingRight;
+
+                right.gameObject.SetActive(false);
+                left.gameObject.SetActive(true);
+
+
+            }
+            else if (myRigidbody.velocity.x > 0 && !facingRight)
+            {
+                facingRight = !facingRight;
+                right.gameObject.SetActive(true);
+                left.gameObject.SetActive(false);
+            }
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * 3f, myRigidbody.velocity.y, 0);
+
+        if ( Input.GetAxis("Vertical") > 0 && isGround)
+        {
+
+            myRigidbody.AddForce(new Vector2(0, 125));
+            isGround = false;
+
+
+        }
+        print(myRigidbody.velocity.y);
+
+        if (changeGravity)
+        {
+
+            myRigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * 3f, myRigidbody.velocity.y, 0);
+
+
+
+            transform.DOMoveY(0.5f, 3);
+            if (Input.GetAxis("Vertical") > 0 && isGround)
+            {
+                transform.DOMoveY(-5, 1);
+                //transform.position = transform.position + new Vector3(0, -10, 0);
+              isGround = false;
+               
+
+            }
+        }
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+        
+        
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Teleport")
+        {
+            teleport.Play();
+        }
+        if (collision.gameObject.tag == "ChangeGravity")
+        {
+            changeGravity = true;
+            platformAfterGravityChange.gameObject.SetActive(true);
+            transform.eulerAngles = new Vector3(0, 0, 180);
+
+        }
+        if (collision.gameObject.tag == "Wind")
+        {
+            transform.DOMove(new Vector3(0, 15, 0), 3).SetEase(easetipi);
+            
+        }
+
+    }
+    
+}
---
+++ Oyunla Gelecek/Assets/Scripts/cam2d.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam2d : MonoBehaviour
+{
+    //Object you want to follow
+    public Transform target;
+
+    //Set to z -10
+    public Vector3 offset;
+
+    //How much delay on the follow
+    [Range(0, 10)]
+    public float smoothing;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
